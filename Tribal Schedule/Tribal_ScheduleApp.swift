aecf89fdb1412770ea960052572d75974f73caf1
Chi <richardmlfns@gmail.com>
2024-10-12 23:01:39 -0700

    Initial Commit

---
+++ Tribal Schedule/Tribal_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tribal_ScheduleApp.swift
+//  Tribal Schedule
+//
+//  Created by 张驰 on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Tribal_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tribal ScheduleTests/Tribal_ScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tribal_ScheduleTests.swift
+//  Tribal ScheduleTests
+//
+//  Created by 张驰 on 10/12/24.
+//
+
+import Testing
+@testable import Tribal_Schedule
+
+struct Tribal_ScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
